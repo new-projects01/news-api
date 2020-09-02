cab2c1a57fe960026ab077d23394bf5ec4c19355
Ryan Harley <rharley.dev@gmail.com>
2020-09-02 12:22:56 -0500

    successful ping of api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getNewsIds } from './services/newsAPI';
 
 function App() {
-  return <div>Hello</div>;
+  const [newsIds, setNewsIds] = useState([]);
+
+  useEffect(() => {
+    console.log(getNewsIds());
+  }, []);
+
+  return <div>{newsIds}</div>;
 }
 
 export default App;
