9a0f40869a5c6c1bfce6a39a92aaa721ed2eaf7a
Ryan Harley <rharley.dev@gmail.com>
2020-09-09 10:44:01 -0500

    styling changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NewsContextProvider from './components/NewsContext';
 import News from './components/News';
-import './app.css';
+import './App.css';
 
 function App() {
   return (
--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -3,11 +3,16 @@ import React from 'react';
 function NewsArticle({ data }) {
   return (
     <div className="news">
-      <a href={data.url} target="_blank"><img className="image" src={data.urlToImage}></img></a>
-      <a href={data.url} target="_blank"><h2 className="title">{data.title}</h2></a>
-      <p className="author">{data.author}</p>
+      <a href={data.url} target="_blank" rel="noopener noreferrer">
+        <img className="image" src={data.urlToImage} alt="article"></img>
+      </a>
+      <a href={data.url} target="_blank" rel="noopener noreferrer">
+        <h2 className="title">{data.title}</h2>
+      </a>
+      <br />
+      <br />
       <p className="published">{data.publishedAt}</p>
-      <p className="source">{data.source.name}</p>
+      <p className="source">Source: {data.source.name} from {data.author}</p>
     </div>
   );
 }
